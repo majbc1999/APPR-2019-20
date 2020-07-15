f3227f5cb70afb884fc859be06aa39590a44b7c1
majbc1999 <majbc1999@gmail.com>
2020-07-16 00:47:23 +0200

    manjši popravki pred dejanskim začetkom dela

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,12 +1,12 @@
 # ANALIZA CENE ELEKTRIČNE ENERGIJE IN ZEMELJSKEGA PLINA
 
 uvozi_cene_elektrike <- function(){
-  cene_elektrike <- read_csv('podatki/cena_elektricna_energija.csv',col_names=TRUE,locale=locale(encoding='UTF-8'))
+  cene_elektrike <- read_csv('podatki/cena_elektricna_energija.csv',col_names=TRUE,locale=locale(encoding='Windows-1250'))
   return(cene_elektrike)
 }
 
 uvozi_cene_plina <- function(){
-  cene_plina <- read_csv('podatki/cena_zemeljski_plin.csv',col_names=TRUE,locale=locale(encoding='UTF-8'))
+  cene_plina <- read_csv('podatki/cena_zemeljski_plin.csv',col_names=TRUE,locale=locale(encoding='Windows-1250'))
   return(cene_plina)
 }
 
@@ -14,7 +14,7 @@ uvozi_cene_plina <- function(){
 # ANALIZA PRIMERJAVE PORABE PO GOSPODINJSTVIH
 
 uvozi_primerjavo <- function(){
-  tabela_primerjav <- read_csv('podatki/primerjava_porabe_po_gospodinjstvih.csv',col_names=TRUE,locale=locale(encoding='UTF-8'))
+  tabela_primerjav <- read_csv('podatki/primerjava_porabe_po_gospodinjstvih.csv',col_names=TRUE,locale=locale(encoding='Windows-1250'))
   return(tabela_primerjav)
 }
 
