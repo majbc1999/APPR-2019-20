6c7e34b7c2e4db1b02027fd1759c821d7d98a17d
majbc1999 <majbc1999@gmail.com>
2020-07-30 23:29:39 +0200

    Dodan še en graf

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -8,6 +8,21 @@ zemljevid <- uvozi.zemljevid(
 
 TD_cena_energentov$leto_in_cetrtletje <- as.numeric(as.character(TD_cena_energentov$leto_in_cetrtletje))
 
+
+# Ustvarjanje novih podatkov
+samo_elektrika <- TD_cena_energentov %>% filter(energent == "Električna energija")
+names(samo_elektrika) <- c("energent", "leto", "cena_elektrika")
+samo_plin <- TD_cena_energentov %>% filter(energent == "Zemeljski plin")
+names(samo_plin) <- c("energent", "leto", "cena_plin")
+primerjava_cen <- merge(samo_elektrika,samo_plin,by="leto")
+primerjava_cen[,c(2,4)] <- NULL
+primerjava_cen$razlika <- with(primerjava_cen, cena_elektrika - cena_plin)
+
+# Tukaj so sedaj grafi
+
 graf1 <- ggplot(data=TD_cena_energentov, aes(x=leto_in_cetrtletje, y=`cena_(EUR/kWh)`, col=energent)) + 
-  geom_point() + geom_line() +xlab('leto') + ylab('cena (EUR/kWh)') + ggtitle('Gibanje cene nafte in plina skozi čas') + 
-  theme(axis.text.x=element_text(angle=45, vjust=0.5, hjust=1))
+  geom_point() + geom_line() +xlab('leto') + ylab('cena (EUR/kWh)') + ggtitle('Gibanje cene električne energije in plina skozi čas')
+
+
+graf2 <- ggplot(data=primerjava_cen, aes(x=leto, y=razlika)) + 
+  geom_path(color="blue") + xlab('leto') + ylab('cena elektrike - cena plina (EUR/kWh)') + ggtitle('Razlika cen elektrike in plina skozi čas')
