e72eba37d399f46dcccf104218734eb14b6c4b53
majbc1999 <majbc1999@gmail.com>
2020-07-30 14:08:02 +0200

    Sedaj dela geom_line()

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -4,6 +4,10 @@
 zemljevid <- uvozi.zemljevid(
   "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip", "ne_50m_admin_0_countries", encoding="UTF-8")
 
+# Pretvorimo v numeriko
+
+TD_cena_energentov$leto_in_cetrtletje <- as.numeric(as.character(TD_cena_energentov$leto_in_cetrtletje))
+
 graf1 <- ggplot(data=TD_cena_energentov, aes(x=leto_in_cetrtletje, y=`cena_(EUR/kWh)`, col=energent)) + 
-  geom_point() + xlab('leto') + ylab('cena (EUR/kWh') + ggtitle('Gibanje cene nafte in plina skozi čas') + 
+  geom_point() + geom_line() +xlab('leto') + ylab('cena (EUR/kWh)') + ggtitle('Gibanje cene nafte in plina skozi čas') + 
   theme(axis.text.x=element_text(angle=45, vjust=0.5, hjust=1))
