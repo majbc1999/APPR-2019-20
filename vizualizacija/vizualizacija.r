0e8843db72024147902d3d733338a63a6b625088
majbc1999 <majbc1999@gmail.com>
2020-08-01 23:41:03 +0200

    Narisan area_plot in pie_plot

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -7,7 +7,8 @@ zemljevid <- uvozi.zemljevid(
 # Pretvorimo v numeriko
 
 TD_cena_energentov$leto_in_cetrtletje <- as.numeric(as.character(TD_cena_energentov$leto_in_cetrtletje))
-
+TD_slo_obnovljivi$`proizvodnja_(GWh)` <- as.numeric(as.character(TD_slo_obnovljivi$`proizvodnja_(GWh)`))
+TD_slo_obnovljivi[is.na(TD_slo_obnovljivi)] <- 0
 
 # Ustvarjanje novih podatkov
 samo_elektrika <- TD_cena_energentov %>% filter(energent == "Električna energija")
@@ -17,6 +18,7 @@ names(samo_plin) <- c("energent", "leto", "cena_plin")
 primerjava_cen <- merge(samo_elektrika,samo_plin,by="leto")
 primerjava_cen[,c(2,4)] <- NULL
 primerjava_cen$razlika <- with(primerjava_cen, cena_elektrika - cena_plin)
+
 
 # Tukaj so sedaj grafi
 
@@ -30,8 +32,31 @@ graf2 <- ggplot(data=primerjava_cen, aes(x=leto, y=razlika)) +
 #graf3 <- napredna analiza
 
 graf4 <- ggplot(data=TD_poraba_gospodinjstev %>% filter(
-  energetski_vir == "	Utekočinjeni naftni plin (t)" | energetski_vir == "Premog (t)" | 
+  energetski_vir == "Utekočinjeni naftni plin (t)" | energetski_vir == "Premog (t)" | 
   energetski_vir == "Lesni sekanci, briketi in ostanki (t)" | energetski_vir == "Lesni peleti (t)" |
   energetski_vir == "Ekstra lahko kurilno olje (t)" | energetski_vir == "Drva - polena (t)"), aes(x=leto, y=poraba, fill=energetski_vir)) + 
   geom_bar(stat="identity") + xlab('leto') + ylab('letna poraba (t)') + ggtitle('Poraba energetskih virov skozi čas')
 
+graf5 <- ggplot(data=TD_poraba_gospodinjstev %>% filter(
+  energetski_vir == "Toplota iz okolice (TJ)" | energetski_vir == "Sončna energija (TJ)" | 
+  energetski_vir == "Daljinska toplota (TJ)"), aes(x=leto, y=poraba, fill=energetski_vir)) + 
+  geom_bar(stat="identity") + xlab('leto') + ylab('letna poraba (TJ)') + ggtitle('Poraba energetskih virov skozi čas')
+
+#graf6 <- napredna analiza
+
+graf7 <- ggplot(data = TD_slo_obnovljivi %>% filter(
+  elektrarna == "Hidroelektrarne (GWh)" | elektrarna == "Termoelektrarne (GWh)" |
+  elektrarna == "Jedrske elektrarne (GWh)" | elektrarna == "Sončne elektrarne (GWh)"| 
+  elektrarna == "Vetrne elektrarne (GWh)"), aes(x=leto, y=`proizvodnja_(GWh)`, fill=elektrarna)) + 
+  geom_area(aes(group=`elektrarna`)) + xlab('leto') + ylab('proizvodnja (GWh)') + ggtitle('Proizvodnja obnovljive energija po elektrarnah') +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
+
+graf8 <- ggplot(data = TD_slo_obnovljivi %>% filter(
+  leto == 2018, elektrarna == "Hidroelektrarne (GWh)" | elektrarna == "Termoelektrarne (GWh)" |
+    elektrarna == "Jedrske elektrarne (GWh)" | elektrarna == "Sončne elektrarne (GWh)"| 
+    elektrarna == "Vetrne elektrarne (GWh)"), aes(x="", y=`proizvodnja_(GWh)`, fill=elektrarna)) + 
+  geom_bar(width = 1, stat = "identity") + coord_polar("y", start=0) + scale_fill_brewer(palette="Dark2") + 
+  geom_text(aes(label = paste(round(`proizvodnja_(GWh)` / sum(`proizvodnja_(GWh)`) * 100, 1), "%")), position = position_stack(vjust = 0.5)) +
+  ggtitle('Tortni diagram za proizvodnjo obnovljive energije z odstotki za leto 2018') + theme(axis.text = element_blank(),
+  axis.ticks = element_blank(), panel.grid  = element_blank(), axis.title.x = element_blank(),
+  axis.title.y = element_blank())
